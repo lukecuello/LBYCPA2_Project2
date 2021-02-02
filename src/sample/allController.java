8f1bff554b5da7ae8c1922009458ca38e7183e0d
lukecuello <60372924+lukecuello@users.noreply.github.com>
2021-02-03 03:21:11 +0800

    Edited, will finish sorting tomorrow

--- src/sample/allController.java
+++ src/sample/allController.java
@@ -4,9 +4,17 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.beans.EventHandler;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 
 public class allController {
@@ -19,22 +27,64 @@ public class allController {
 
     String[] choices = {"Month", "Price", "Expense"};
 
+    HashMap<String, LinkedList<ArrayList<String>>> adjList = new HashMap<>();
 
-    public void sortBy(){
-        for(int i = 0; i<choices.length;i++){
-            sort.getItems().add(choices[i]);
-        }
-    }
+
+
     public void initialize(){
-       //Sorting algorithm to be displayed on the List View
+        for(String i:choices){
+            sort.getItems().add(i);
+        }
+        sort.setOnAction((Action) -> {
+            String selected = sort.getSelectionModel().getSelectedItem();
+            if (selected.equals("Month")) {
+                lv.getItems().clear();
+                lv.getItems().addAll("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+                lv.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+            } else if (selected.equals("Price")) {
+                lv.getItems().clear();
+                lv.getItems().addAll("100,000", "50,000", "75,000", "10,000", "5,000", "1,000", "500", "300");
+                lv.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+            } else if (selected.equals("Expense")) {
+
+            } else {
+                lv.getItems().clear();
+                lv.getItems().add("None");
+                lv.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+            }
+        });
     }
 
     public void displaySelected(){
-        String selected = lv.getSelectionModel().getSelectedItem();
-        if(selected.equals("woop")){
-            screen.setText("HAHAHAH");
-        }
+        //For showing the expenses
     }
+
+//    public void clickBubbleSort()
+//    {
+//        boolean swapped = true;
+//        int a;
+//        int b = 0;
+//        StackPane tmp;
+//        while (swapped)
+//        {
+//            swapped = false;
+//            b++;
+//            for (a = 0; a < stackPane.length - b; a++)
+//            {
+//                if (getNum(getText(stackPane[a]).getText()) > getNum(getText(stackPane[a+1]).getText()))
+//                {
+//                    swapPanes(stackPane[a], stackPane[a+1]);
+//                    tmp = stackPane[a];
+//                    stackPane[a] = stackPane[a + 1];
+//                    stackPane[a + 1] = tmp;
+//                    swapped = true;
+//                }
+//            }
+//        }
+//    }
+
+
+
 
     public void goBack() throws IOException {
         Main app = new Main();
