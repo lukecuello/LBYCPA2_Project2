9779e7d43f3c5001048e363122b2e0812c6bbb47
lukecuello <60372924+lukecuello@users.noreply.github.com>
2021-02-03 14:39:48 +0800

    Finishing all expenses controller

--- src/sample/allController.java
+++ src/sample/allController.java
@@ -10,6 +10,7 @@ import javafx.scene.layout.StackPane;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
 import java.beans.EventHandler;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -26,6 +27,9 @@ public class allController {
     ChoiceBox<String> sort;
 
     String[] choices = {"Month", "Price", "Expense"};
+    String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+    String[] prices = {"100,000","75,000", "50,000","25,000", "10,000", "5,000", "1,000", "500", "300"};
+    String[] expense = {"Electricity", "Water", "Groceries", "Tax", "Insurance"};
 
    public Profile profile;
 
@@ -39,14 +43,16 @@ public class allController {
             String selected = sort.getSelectionModel().getSelectedItem();
             if (selected.equals("Month")) {
                 lv.getItems().clear();
-                lv.getItems().addAll("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+                lv.getItems().addAll(months);
                 lv.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
             } else if (selected.equals("Price")) {
                 lv.getItems().clear();
-                lv.getItems().addAll("100,000", "50,000", "75,000", "10,000", "5,000", "1,000", "500", "300");
+                lv.getItems().addAll(prices);
                 lv.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
             } else if (selected.equals("Expense")) {
-
+                lv.getItems().clear();
+                lv.getItems().addAll(expense);
+                lv.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
             } else {
                 lv.getItems().clear();
                 lv.getItems().add("None");
@@ -56,32 +62,48 @@ public class allController {
     }
 
     public void displaySelected(){
-        //For showing the expenses
+       String selected = lv.getSelectionModel().getSelectedItem();
+       for(int i =0; i < months.length;i++){
+           if(selected.equals(months[i]) || selected.equals(expense[i])){
+               lv.setOnContextMenuRequested(e -> perMonth());
+           }
+           else if(selected.equals(prices[i])){
+               lv.setOnContextMenuRequested(e -> expenseBubbleSort());
+           }
+           else{
+               continue;
+           }
+       }
+
     }
 
-//    public void clickBubbleSort()
-//    {
-//        boolean swapped = true;
-//        int a;
-//        int b = 0;
-//        StackPane tmp;
-//        while (swapped)
-//        {
-//            swapped = false;
-//            b++;
-//            for (a = 0; a < stackPane.length - b; a++)
-//            {
-//                if (getNum(getText(stackPane[a]).getText()) > getNum(getText(stackPane[a+1]).getText()))
-//                {
-//                    swapPanes(stackPane[a], stackPane[a+1]);
-//                    tmp = stackPane[a];
-//                    stackPane[a] = stackPane[a + 1];
-//                    stackPane[a + 1] = tmp;
-//                    swapped = true;
-//                }
-//            }
-//        }
-//    }
+    public void perMonth(){
+
+    }
+
+    public void expenseBubbleSort()
+    {
+        boolean swapped = true;
+        int a;
+        int b = 0;
+        StackPane tmp;
+        while (swapped)
+        {
+            swapped = false;
+            b++;
+            for (a = 0; a < stackPane.length - b; a++)
+            {
+                if (getNum(getText(stackPane[a]).getText()) > getNum(getText(stackPane[a+1]).getText()))
+                {
+                    swapPanes(stackPane[a], stackPane[a+1]);
+                    tmp = stackPane[a];
+                    stackPane[a] = stackPane[a + 1];
+                    stackPane[a + 1] = tmp;
+                    swapped = true;
+                }
+            }
+        }
+    }
 
 
 
